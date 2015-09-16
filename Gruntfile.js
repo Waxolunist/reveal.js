315de5a0ea1b0d276676bafb403a4d714e3dc8ae
Sterzl, Christian <Christian.Sterzl@sympany.ch>
2015-09-16 13:22:40 +0200

    Disable test by default

--- Gruntfile.js
+++ Gruntfile.js
@@ -165,7 +165,7 @@ module.exports = function (grunt) {
   grunt.registerTask('default', ['css', 'js', 'img']);
 
   // JS task
-  grunt.registerTask('js', ['jshint', 'uglify', 'qunit']);
+  grunt.registerTask('js', ['uglify']);
 
   // Theme CSS
   grunt.registerTask('css-themes', ['sass:themes']);
